0afab969dca6bffb1c81066948adbd8bf1cc066e
Gerard Viñuela <gerardviru@gmail.com>
2022-04-06 13:20:43 +0200

    Update

--- UD04/src/UD04/Ejercicio4.java
+++ UD04/src/UD04/Ejercicio4.java
@@ -4,8 +4,18 @@ public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		
-		int n=5;
-
+		int n=10;
+		
+		System.out.println("Valor inicial de n = "+n);
+		
+		n+=77;
+		System.out.println("N + 77 = "+n);
+		
+		n-=3;
+		System.out.println("N - 3 = "+n);
+		
+		n*=2;
+		System.out.println("N * 2 = "+n);
 	}
 
 }
